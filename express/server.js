fff464124a3da0c610525d2b490ae8e4dcacda7f
Marsel Salimkhanov <marselteles7@gmail.com>
2022-05-07 00:23:04 +0500

    fix

--- express/server.js
+++ express/server.js
@@ -5,7 +5,17 @@ const querystring = require('querystring');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
+const moment = require('moment-timezone')
+const { Telegraf, Markup } = require('telegraf')
 let ordersChatId = -507976908;
+
+const bot = new Telegraf('5262311380:AAEZkvr3DUuIQ5DVUeniWduH9rWIMqFS3sk')
+bot.start((ctx) => {
+  console.log(ctx)
+  ctx.reply('Welcome to burger')
+})
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
 
 const router = express.Router();
 router.get('/', (req, res) => {
